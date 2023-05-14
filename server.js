8010faf068d778535b69bbd5ebcb7103a2b8f1d2
Sezer Mehmed <sezer20001@gmail.com>
2023-05-15 02:08:31 +0300

    Add validations for methods

--- server.js
+++ server.js
@@ -8,44 +8,78 @@ const upload = multer({ dest: 'uploads/' });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', upload.single('file'), async (req, res) => {
+
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file provided' });
+  }
+
   const originalFilename = req.file.originalname;
+
+  const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt', '.csv', '.mp3', '.wav', '.mp4', '.mov', '.zip', '.tar', '.gz'];
   const originalExtension = path.extname(originalFilename);
+
+  if (!allowedExtensions.includes(originalExtension)) {
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    return res.status(400).json({ error: 'Invalid file type' });
+  }
 
   const newFilename = `${req.file.filename}${originalExtension}`;
   const oldPath = path.join(__dirname, 'uploads', req.file.filename);
   const newPath = path.join(__dirname, 'uploads', newFilename);
-  fs.renameSync(oldPath, newPath);
 
-  res.json({
-    message: 'File uploaded successfully!',
-    filename: newFilename,
-  });
+  try {
+    fs.rename(oldPath, newPath);
+    res.json({
+      message: 'File uploaded successfully!',
+      filename: newFilename,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to upload the file' });
+  }
 });
 
-app.get('/uploads', (req, res) => {
-    fs.readdir('uploads/', (err, files) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Error reading files' });
-      } else {
-        res.json({ files });
-      }
-    });
-  });
+app.get('/uploads', async (req, res) => {
+  try {
+    const files = await fs.promises.readdir('uploads/');
+    res.json({ files });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error reading files' });
+  };
+});
 
-app.get('/uploads/:filename', (req, res) => {
+app.get('/uploads/:filename', async (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(__dirname, 'uploads', filename);
 
-  const contentType = getContentType(filename);
-  res.contentType(contentType);
+  try {
+    const fileExists = await fs.promises.access(filePath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
 
-  res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+    if (!fileExists) {
+      return res.status(404).json({ error: 'File not found' });
+    }
 
-  const fileStream = fs.createReadStream(filePath);
-  fileStream.pipe(res);
+    const contentType = getContentType(filename);
+    res.contentType(contentType);
+
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to retrieve the file' });
+  }
 });
+
 
 function getContentType(filename) {
   const ext = path.extname(filename);
@@ -95,32 +129,55 @@ function getContentType(filename) {
   }
 }
 
-app.delete('/delete/:filename', (req, res) => {
+app.delete('/delete/:filename', async (req, res) => {
   const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'uploads', filename);
 
-  fs.unlink(`uploads/${filename}`, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to delete the file.' });
-    } else {
-      res.json({ message: `File "${filename}" deleted successfully!` });
+  try {
+    const fileExists = await fs.promises.access(filePath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!fileExists) {
+      return res.status(404).json({ error: 'File not found' });
     }
-  });
+
+    await fs.promises.unlink(filePath);
+    res.json({ message: `File "${filename}" deleted successfully!` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to delete the file' });
+  };
 });
 
-app.put('/rename/:filename', (req, res) => {
+app.put('/rename/:filename', async (req, res) => {
   const oldFilename = req.params.filename;
   const newFilename = req.body.newFilename;
 
-  fs.rename(`uploads/${oldFilename}`, `uploads/${newFilename}`, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to rename the file.' });
-    } else {
-      res.json({ message: `File "${oldFilename}" renamed to "${newFilename}" successfully!` });
+  if (!newFilename) {
+    return res.status(400).json({ error: 'New filename is required' });
+  }
+
+  const oldFilePath = path.join(__dirname, 'uploads', oldFilename);
+  const newFilePath = path.join(__dirname, 'uploads', newFilename);
+
+  try {
+    const fileExists = await fs.promises.access(oldFilePath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!fileExists) {
+      return res.status(404).json({ error: 'File not found' });
     }
-  });
+
+    await fs.promises.rename(oldFilePath, newFilePath);
+    res.json({ message: `File "${oldFilename}" renamed to "${newFilename}" successfully!` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to rename the file' });
+  };
 });
+
 
 const port = 3000; 
 app.listen(port, () => {
