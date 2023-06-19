0c214276ce2f7d317e6de9feecb81bfc26c135cb
Sezer Mehmed <sezer20001@gmail.com>
2023-06-19 16:17:29 +0300

    Fix

--- db.js
+++ db.js
@@ -10,28 +10,28 @@ async function getUserById(id) {
     return await db.oneOrNone('SELECT * FROM webtech.userDB WHERE id = $1', [id]);
 }
 
-async function createFolder(name, parentFolderId, userId) {
-  return await db.one('INSERT INTO webtech.folderDB (name, parentFolderId, userId) VALUES ($1, $2, $3) RETURNING *', [name, parentFolderId, userId]);
-}
+// async function createFolder(name, parentFolderId, userId) {
+//   return await db.one('INSERT INTO webtech.folderDB (name, parentFolderId, userId) VALUES ($1, $2, $3) RETURNING *', [name, parentFolderId, userId]);
+// }
 
-async function getFolder(id) {
-  return await db.one('SELECT * FROM webtech.folderDB WHERE id = $1', [id]);
-}
+// async function getFolder(id) {
+//   return await db.one('SELECT * FROM webtech.folderDB WHERE id = $1', [id]);
+// }
 
-async function updateFolder(id, name, parentFolderId) {
-  return await db.none('UPDATE webtech.folderDB SET name = $1, parentFolderId = $2 WHERE id = $3', [name, parentFolderId, id]);
-}
+// async function updateFolder(id, name, parentFolderId) {
+//   return await db.none('UPDATE webtech.folderDB SET name = $1, parentFolderId = $2 WHERE id = $3', [name, parentFolderId, id]);
+// }
 
-async function deleteFolder(id) {
-  return await db.none('DELETE FROM webtech.folderDB WHERE id = $1', [id]);
-}
+// async function deleteFolder(id) {
+//   return await db.none('DELETE FROM webtech.folderDB WHERE id = $1', [id]);
+// }
 
   module.exports = {
     db,
     getUserByUsername,
-    getUserById,
-    createFolder,
-    getFolder,
-    updateFolder,
-    deleteFolder
+    getUserById
+    // createFolder,
+    // getFolder,
+    // updateFolder,
+    // deleteFolder
   };
--- server.js
+++ server.js
@@ -180,14 +180,18 @@ app.put('/api/rename',passport.authenticate('jwt', { session: false }), async (r
     return res.status(400).json({ error: 'New filename is required' });
   }
 
+  const oldExtension = path.extname(oldFilename);
+  const newFilenameWithExtension = `${newFilename}${oldExtension}`;
+
   const oldFilePath = path.join(__dirname, 'uploads', oldFilename);
-  const newFilePath = path.join(__dirname, 'uploads', newFilename);
+  const newFilePath = path.join(__dirname, 'uploads', newFilenameWithExtension);
 
   try {
     await fs.promises.access(oldFilePath, fs.constants.F_OK)
     await fs.promises.rename(oldFilePath, newFilePath);
-    await db.none('UPDATE webtech.files SET filename = $1 WHERE filename = $2', [newFilename, oldFilename]);
-    res.json({ message: `File "${oldFilename}" renamed to "${newFilename}" successfully!` });
+    await db.none('UPDATE webtech.files SET filename = $1 WHERE filename = $2', [newFilenameWithExtension, oldFilename]);
+
+    res.json({ message: `File "${oldFilename}" renamed to "${newFilenameWithExtension}" successfully!`});
   } catch (error) {
     res.status(404).json({ error: 'File not found' });
   };
@@ -267,192 +271,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-// require('dotenv').config();
-// const express = require('express');
-// const multer = require('multer');
-// const fs = require('fs');
-// const path = require('path'); 
-// const app = express();
-// const upload = multer({ dest: 'uploads/' });
-// const bcrypt = require('bcryptjs');
-// const passport = require('./passport');
-// const dbModule = require('./db');
-// const db = dbModule.db
-// const jwt = require('jsonwebtoken');
-// const queries = require('./queries');
-// const cors = require('cors');
-// const router = express.Router();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(passport.initialize());
-
-// function isValidEmail(email) {
-//   const regex = /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//   return regex.test(email);
-// }
-
-// app.post('/register', async (req, res) => {
-//   const { username, email, password } = req.body;
-
-//   try {
-//     const existingUserByUsername = await db.oneOrNone(queries.checkIfUsernameExists, [username]);
-//     const existingUserByEmail = await db.oneOrNone(queries.checkIfEmailExists, [email]);
-
-//     if (!isValidEmail(email)) {
-//       return res.status(400).json({ error: 'Invalid email' });
-//     }
-
-//     if (existingUserByUsername) {
-//       return res.status(400).json({ error: 'Username already exists' });
-//     }
-
-//     if (existingUserByEmail) {
-//       return res.status(400).json({ error: 'Email already exists' });
-//     }
-
-//     if (password.length < 8) {
-//       return res.status(400).json({ error: 'Password should be at least 8 characters long' });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     await db.none('INSERT INTO webtech.userDB (username, email, password) VALUES ($1, $2, $3)', [username, email, hashedPassword]);
-
-//     res.json({ message: 'User registered successfully!' });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Failed to register user', message: err.message });
-//   }
-// });
-
-// app.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
-//   const user = req.user;
-//   const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET, {expiresIn: '1h'});
-//   res.json({ token });
-// });
-
-// app.post('/api/upload', passport.authenticate('jwt', { session: false }), upload.single('file'), async (req, res) => {
-//   console.log(req.user);
-//   if (!req.file) {
-//     return res.status(400).json({ error: 'No file provided' });
-//   }
-
-//   const originalFilename = req.file.originalname;
-
-//   const allowedExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.txt', '.csv', '.mp3', '.wav', '.mp4', '.mov', '.zip', '.tar', '.gz'];
-//   const originalExtension = path.extname(originalFilename);
-
-//   if (!allowedExtensions.includes(originalExtension)) {
-//     await fs.promises.unlink(req.file.path);
-//     return res.status(400).json({ error: 'Invalid file type' });
-//   }
-
-//   const newFilename = `${req.file.filename}${originalExtension}`;
-//   const oldPath = path.join(__dirname, 'uploads', req.file.filename);
-//   const newPath = path.join(__dirname, 'uploads', newFilename);
-
-//   try {
-//     await fs.promises.rename(oldPath, newPath);
-//     const metadata = {
-//       userId: req.user.id,
-//       filename: newFilename,
-//       originalName: originalFilename,
-//       size: req.file.size,
-//       uploadDate: new Date().toISOString(),
-//       mimeType: req.file.mimetype,
-//       path: newPath,
-//       parentFolderId: req.body.parentFolderId
-//     };
-
-//     await db.none('INSERT INTO webtech.files (userId, filename, originalName, size, uploadDate, mimeType, path, parentFolderId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [metadata.userId, metadata.filename, metadata.originalName, metadata.size, metadata.uploadDate, metadata.mimeType, metadata.path, metadata.parentFolderId]);
-//     res.status(201).json({
-//       message: 'File uploaded successfully!',
-//       filename: newFilename,
-//     });
-//   } catch (error) {
-//     res.status(400).json({ error: 'Failed to upload the file' });
-//   }
-// });
-
-// app.get('/api/uploads', passport.authenticate('jwt', { session: false }), async (req, res) => {
-//   try {
-//     const files = await db.any('SELECT * FROM webtech.files WHERE userId = $1 AND parentFolderId = $2', [req.user.id, req.query.parentFolderId]);
-//     res.json({ files });
-//   } catch (error) {
-//     res.status(400).json({ message: 'Error reading files' });
-//   };
-// });
-
-// app.get('/api/uploads/:filename', passport.authenticate('jwt', { session: false }), async (req, res) => {
-//   const filename = req.params.filename;
-//   const filePath = path.join(__dirname, 'uploads', filename);
-//   const fileMetadata = await db.oneOrNone('SELECT * FROM webtech.files WHERE filename = $1', [filename]);
-
-//   if (!fileMetadata) {
-//     return res.status(404).json({ error: 'File not found in metadata' });
-//   }
-
-//   if (fileMetadata.userid !== req.user.id) {
-//     return res.status(403).json({ error: 'Access denied' });
-//   }
-
-//   try {
-//     await fs.promises.access(filePath, fs.constants.F_OK)
-//     const contentType = getContentType(filename);
-//     res.contentType(contentType);
-//     res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
-//     const fileStream = fs.createReadStream(filePath);
-//     fileStream.pipe(res);
-//   } catch (error) {
-//     res.status(404).json({ error: 'File not found' });
-//   }
-// });
-
-// app.delete('/api/delete',passport.authenticate('jwt', { session: false }), async (req, res) => {
-//   const filename = req.body.filename;
-//   const filePath = path.join(__dirname, 'uploads', filename);
-//   const fileMetadata = await db.oneOrNone('SELECT * FROM webtech.files WHERE filename = $1', [filename]);
-
-//   if (!fileMetadata) {
-//     return res.status(404).json({ error: 'File not found in metadata' });
-//   }
-
-//   if (fileMetadata.userid !== req.user.id) {
-//     return res.status(403).json({ error: 'Access denied' });
-//   }
-
-//   try {
-//     await fs.promises.access(filePath, fs.constants.F_OK)
-//     await fs.promises.unlink(filePath);
-//     await db.none('DELETE FROM webtech.files WHERE filename = $1', [filename]);
-//     res.json({ message: `File "${filename}" deleted successfully!` });
-//   } catch (error) {
-//     res.status(404).json({ error: 'File not found' });
-//   }
-// });
-
-// app.post('/api/folder', passport.authenticate('jwt', { session: false }), async (req, res) => {
-//   const { name, parentFolderId } = req.body;
-
-//   try {
-//     await db.none('INSERT INTO webtech.folderDB (name, parentFolderId, userId) VALUES ($1, $2, $3)', [name, parentFolderId, req.user.id]);
-//     res.json({ message: 'Folder created successfully!' });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Failed to create folder', message: err.message });
-//   }
-// });
-
-// app.get('/api/folder', passport.authenticate('jwt', { session: false }), async (req, res) => {
-//   try {
-//     const folders = await db.any('SELECT * FROM webtech.folderDB WHERE userId = $1 AND parentFolderId = $2', [req.user.id, req.query.parentFolderId]);
-//     res.json({ folders });
-//   } catch (error) {
-//     res.status(400).json({ message: 'Error reading folders' });
-//   };
-// });
-
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`Server is running on port ${process.env.PORT || 3000}`);
-// });
